8199a917ee5b8e9f59163668e972f2233244e2d2
Ahnaf Chowdhury <ahnafchowdhury@endpoint-10-192-50-188.ufvisitor.ufl.edu>
2026-01-24 14:31:16 -0500

    babel config fix v2

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,4 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      "nativewind/babel",
-      ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }],
-    ],
-  };
+module.exports = {
+  presets: ["babel-preset-expo"],
 };
 
