7cc848d35470244ef1139b5eaf15f96b637cd0c9
DigitalHail <liamsgale@gmail.com>
2026-01-24 16:18:49 -0500

    fixing errors of overhaul

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++ .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
@@ -2,8 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/app_globals_71f961d1.css",
-  "static/chunks/node_modules_41609f47._.js",
-  "static/chunks/app_providers_tsx_7ef36779._.js"
+  "static/chunks/_4a15ba0b._.js"
 ],
     source: "dynamic"
 });
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Index() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-xl font-bold">Welcome to GatorGreen</h1>
+    </div>
+  );
+}
