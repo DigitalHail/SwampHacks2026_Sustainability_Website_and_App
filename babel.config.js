3b8f3fbb29bd9716d7b006a62068ea0586d88018
Ae-cat <ariellaiki@gmail.com>
2026-01-24 14:21:34 -0500

    Home

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      "nativewind/babel",
-      ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }],
-    ],
+    presets: ["babel-preset-expo", "nativewind/babel"],
+    plugins: [["@babel/plugin-transform-react-jsx", { runtime: "automatic" }]],
   };
 };
 
